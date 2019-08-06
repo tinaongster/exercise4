08a86dc1be276f4e6c5d7f03e4d209eeafda87b2
tinaongster <52726695+tinaongster@users.noreply.github.com>
2019-08-06 19:59:55 -0400

    Add files via upload

---
+++ assets/data/transportation.ts
@@ -0,0 +1,19 @@
+import { vehicles } from '../../app/vehicles';
+
+export const TRANSPORTATION: vehicles[] = [
+    {
+        car: "BMW",
+        bus: "TTC",
+    },
+
+    {
+        car: "Mercedes",
+        bus: "GO Bus",
+    },
+
+    {
+        car: "Audi",
+        bus: "YRT",
+    },
+
+]
